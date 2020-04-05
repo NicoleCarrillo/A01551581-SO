fc6fc23da490b5c81ea852d8ef8180088bc87ca8
Nicole Carrllo <A01551581@itesm.mx>
2020-04-05 02:13:00 +0000

    proyecto mates compus final

--- PROYECTO/Objeto.java
+++ PROYECTO/Objeto.java
@@ -1,39 +1,18 @@
-//CLASE -> "OBJETO"
-//AQUI DIVIDO MI EXPRESION REGULAR EN MULTIPLES UNIDADES QUE PERTENECEN AL MISMO OBJETO
-//CADA UNIDAD SERA SUSTITUIDA POR LA CADENA DE SUSTITUCION CADA QUE ENCUENTRE UNA SIMILITUD EN LA CADENA 
-
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Stack;
 
 public class Objeto {
-	
 	public String expresion; 	  		  //NUESTRA EXPRESION REGULAR 
 	public String repetidor;     		 //REPETIDOR ES EN EL CASO QUE TENGA UN ASTERISCO *
-	public Objeto repetidorMulti;    //REPETIDOR ES EN EL CASO QUE TENGA UN ASTERISCO * QUE ENGLOBE PARENTESIS ( )*
 	public static String reemplazo;
-	
 	public List<Objeto> solucion=new ArrayList<Objeto>();   //CADA OBJETO TIENE SU LISTA DE SOLUCIONES
-	public List<Parentesis> listaParentesis;   //CADA OBJETO TIENE SU LISTA DE PARENTESIS 
-	
-	public boolean banderaSuma=false;		//SI HAY + QUE REPRESENTA UNA UNION SE PRENDE LA BANDERA Y TRATA LAS SOLUCIONES DE MANERA DIFERENTE
-	public boolean banderaConcatenacion=false;	
 	public boolean banderaC=false;	
-	public boolean banderaCasilla=false;
 	public String resultado="";
-	
-	//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	
+
 	public Objeto(String expresion,String repetidor) {
 		this.expresion=expresion;
 		this.repetidor=repetidor;
 	}
-
-	public Objeto(Objeto repetidorMulti) {
-		this.expresion=null;
-		this.repetidorMulti=repetidorMulti;
-	}
-
 
 	public Objeto(String expresion) {
 		this.expresion=expresion;
@@ -41,102 +20,40 @@ public class Objeto {
 		recorrer();
 	}
 
-	public void parentesis() {
-		Stack<Character> stackLetras = new Stack<Character>();
-
-		char arrayDeParentesis[] =expresion.toCharArray();
-		List<Integer> posiciones=new <Integer>ArrayList();
-		listaParentesis=new <Parentesis>ArrayList();
-		int i;
-		for (i = 0; i < arrayDeParentesis.length; i++) {
-			if (arrayDeParentesis[i] == '(') {
-				stackLetras.push(arrayDeParentesis[i]);
-				posiciones.add(i);
-			}else if (arrayDeParentesis[i] == ')') {
-				if (!stackLetras.empty() && stackLetras.peek() != ')') {
-					stackLetras.pop();
-					Parentesis p = new Parentesis((int) posiciones.get(posiciones.size()-1),i);
-					listaParentesis.add(p);
-					posiciones.remove((int)posiciones.size()-1);
-				}else{
-					stackLetras.push(arrayDeParentesis[i]);  
-				}
-			}
-		}
-
-	}
-	
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	public void recorrer() {     //AGREGAR EL COSO SOLITO A 
 		String concatenacion="";
-		
 		for(int x=0;x<expresion.length();x++) {
-
 			if(x==expresion.length()-1) {   //AGREGAMOS ULTIMO
 				if(expresion.charAt(x)!='*') {
 					concatenacion+=expresion.charAt(x);
 					solucion.add(new Objeto(concatenacion,null));
 				}
 			}
-			else if(expresion.charAt(x)!='(') {
-				if(expresion.charAt(x)!='+') {
-					if((x+1)<expresion.length()&&expresion.charAt(x+1)!='*') {
-						concatenacion+=expresion.charAt(x);
-					}else {
-						if((x+2)<expresion.length()&&expresion.charAt(x+2)!='+') {
-							Objeto ob=new Objeto(concatenacion,String.valueOf(expresion.charAt(x)));
-							ob.banderaC=true;
-							solucion.add(ob);
-							x++; //SALTAMOS *
-							concatenacion="";
-						}else {
-						solucion.add(new Objeto(concatenacion,String.valueOf(expresion.charAt(x))));
+			else if(expresion.charAt(x)!='+') {
+				if((x+1)<expresion.length()&&expresion.charAt(x+1)!='*') {
+					concatenacion+=expresion.charAt(x);
+				}else {
+					if((x+2)<expresion.length()&&expresion.charAt(x+2)!='+') {
+						Objeto ob=new Objeto(concatenacion,String.valueOf(expresion.charAt(x)));
+						ob.banderaC=true;
+						solucion.add(ob);
 						x++; //SALTAMOS *
 						concatenacion="";
-						}
-					}
-				}else {
-					banderaSuma=true;
-					if(concatenacion!="") {
-						solucion.add(new Objeto(concatenacion,null));
+					}else {
+						solucion.add(new Objeto(concatenacion,String.valueOf(expresion.charAt(x))));
+						x++; //SALTAMOS *
 						concatenacion="";
 					}
 				}
 			}else {
 				if(concatenacion!="") {
 					solucion.add(new Objeto(concatenacion,null));
-				}
-				parentesis();
-				Parentesis temporal=null; 
-				for(int y=0;y<listaParentesis.size();y++) {
-					if(listaParentesis.get(y).pInicial==x) {
-						temporal=listaParentesis.get(y);
-					}
-				}
-				
-				if(temporal.pFinal==expresion.length()-1) {
-					solucion.add(new Objeto(expresion.substring(x+1,temporal.pFinal)));
-					x=temporal.pFinal;
-				}else if(expresion.charAt(temporal.pFinal+1)=='+') {
-					banderaSuma=true;
-					solucion.add(new Objeto(expresion.substring(x+1,temporal.pFinal)));
-					x=temporal.pFinal+1;
-				}else if(expresion.charAt(temporal.pFinal+1)=='*') {
-					Objeto multiple=new Objeto(expresion.substring(x+1,temporal.pFinal));
-					solucion.add(new Objeto(multiple));
-					x=temporal.pFinal+1;
-				}else {
-					Objeto objetoConcatenacion=new Objeto(expresion.substring(x+1,temporal.pFinal));
-					objetoConcatenacion.banderaConcatenacion=true;
-					solucion.add(objetoConcatenacion);
-					x=temporal.pFinal;
-					banderaConcatenacion=true;
+					concatenacion="";
 				}
 			}
 		}
 	}
-	
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 	public void metodoNic(String cadena) {
 		int apuntador=0;
 		int pos=0;
@@ -144,8 +61,11 @@ public class Objeto {
 			//TODO ESTO ES PARA SOLUCIONES EN LAS PRIMERAS CASILLAS NO OBJETOS NI ASTERISCOS
 			int tamanio=solucion.size();
 			while(tamanio>apuntador) { //TODO RECORRO ARREGLO DE SOLUCIONES
-				
-				if(solucion.get(apuntador).repetidor==null) { //SI NO TIENES REPETIDOR    CASO A AB A+B
+				if(solucion.get(apuntador).banderaC==true) { //SI SE TRATA DE UNA CONCATENACION
+					String cadena2=metodoNic2(cadena,0,apuntador);
+					apuntador=apuntador+2;
+					cadena=cadena2;
+				}else if(solucion.get(apuntador).repetidor==null) { //SI NO TIENES REPETIDOR    CASO A AB A+B
 					if((pos+solucion.get(apuntador).expresion.length())<=cadena.length()) {  //VALIDACION NO PASAR DE LONGITUD
 						String d=cadena.substring(pos, pos+solucion.get(apuntador).expresion.length());
 						String a=solucion.get(apuntador).expresion;
@@ -187,10 +107,11 @@ public class Objeto {
 							String c=cadena.substring(pos+solucion.get(apuntador).expresion.length(), pos+solucion.get(apuntador).repetidor.length()+solucion.get(apuntador).expresion.length());
 							String e=solucion.get(apuntador).expresion;
 							String r=solucion.get(apuntador).repetidor;
+							int indice2=0;
 							if(e.equals(d)&&r.equals(c)){
 								while(r.equals(c)) {
 									int indice1=pos+solucion.get(apuntador).expresion.length();
-									int indice2=pos+solucion.get(apuntador).repetidor.length()+solucion.get(apuntador).expresion.length();
+									indice2=pos+solucion.get(apuntador).repetidor.length()+solucion.get(apuntador).expresion.length();
 									pos=pos+solucion.get(apuntador).repetidor.length();
 									if(indice2<=cadena.length()) {
 										c=cadena.substring(indice1, indice2);
@@ -199,7 +120,7 @@ public class Objeto {
 									}
 								}
 								System.out.print(reemplazo);	
-								metodoNic(cadena.substring(pos+1));
+								metodoNic(cadena.substring(indice2-1));
 								return;
 							}else if(e.equals(d)){
 								System.out.print(reemplazo);	
@@ -230,14 +151,14 @@ public class Objeto {
 				}
 			}//TERMINA WHILE
 			//EN CASO QUE NO HAYA NADA QUE REEMPLAZAR
-			pos++;
-			System.out.print(cadena.substring(0, 1));
-			metodoNic(cadena.substring(pos));
-			return;
+			if(cadena.length()!=0) {
+				pos++;
+				System.out.print(cadena.substring(0, 1));
+				metodoNic(cadena.substring(pos));
+				return;
+			}
 		} //YA TERMINASTE DE RECORRER TODA LA CADENA 
 	}
-	
-	
 
 	private String metodoNic2(String cadena, int posicionSi,int ap) {
 		int apuntador=ap;
@@ -266,49 +187,48 @@ public class Objeto {
 							}
 							return metodoNic2(cadena.substring(indice2-1),indice2,ap+1);
 						}else if(e.equals(d)){
-							System.out.print(reemplazo);	
 							pos=pos+solucion.get(apuntador).repetidor.length()+solucion.get(apuntador).expresion.length()-1;
-							return metodoNic2(cadena,pos,ap+1);
+							return metodoNic2(cadena.substring(pos),pos,ap+1);
 						}else if(solucion.size()>0){
 							apuntador=apuntador+2;
 						}
 					}else {
 						apuntador=apuntador+2;
 					}
-					}else if(solucion.get(apuntador).repetidor==null) { //NO TIENES REPETIDOR SOLO LA EXPRESION A
-						if((pos+solucion.get(apuntador).expresion.length())<=cadena.length()) {  //VALIDACION NO PASAR DE LONGITUD
-							String d=cadena.substring(pos, pos+solucion.get(apuntador).expresion.length());
-							String a=solucion.get(apuntador).expresion;
-							if(a.equals(d)){	
-								pos=pos+solucion.get(apuntador).expresion.length();
-								return metodoNic2(cadena.substring(pos),pos,ap+1);
-							}else if(solucion.size()>0){
-								apuntador=apuntador+2;
-								posicionSi=0;
-							}
-						}else {
-							apuntador=apuntador+2;
-						}
-					}else if(solucion.get(apuntador).expresion.length()==0) {  //CASO A*  la expresion vacia y repetidor no
-						String d=cadena.substring(pos, pos+solucion.get(apuntador).repetidor.length());
-						String a=solucion.get(apuntador).repetidor;
-						if(a.equals(d)) {
-							while(a.equals(d)) {
-								pos=pos+solucion.get(apuntador).repetidor.length();
-								if(pos<cadena.length()) {
-									d=cadena.substring(pos,pos+solucion.get(apuntador).repetidor.length());
-								}else {
-									d=" ";
-								}
-							}
+				}else if(solucion.get(apuntador).repetidor==null) { //NO TIENES REPETIDOR SOLO LA EXPRESION A
+					if((pos+solucion.get(apuntador).expresion.length())<=cadena.length()) {  //VALIDACION NO PASAR DE LONGITUD
+						String d=cadena.substring(pos, pos+solucion.get(apuntador).expresion.length());
+						String a=solucion.get(apuntador).expresion;
+						if(a.equals(d)){	
+							pos=pos+solucion.get(apuntador).expresion.length();
 							return metodoNic2(cadena.substring(pos),pos,ap+1);
-						}else if(solucion.size()>0) {
+						}else if(solucion.size()>0){
 							apuntador=apuntador+2;
-						}else {
-							apuntador=apuntador+2;
+							posicionSi=0;
 						}
+					}else {
+						apuntador=apuntador+2;
+					}
+				}else if(solucion.get(apuntador).expresion.length()==0) {  //CASO A*  la expresion vacia y repetidor no
+					String d=cadena.substring(pos, pos+solucion.get(apuntador).repetidor.length());
+					String a=solucion.get(apuntador).repetidor;
+					if(a.equals(d)) {
+						while(a.equals(d)) {
+							pos=pos+solucion.get(apuntador).repetidor.length();
+							if(pos<cadena.length()) {
+								d=cadena.substring(pos,pos+solucion.get(apuntador).repetidor.length());
+							}else {
+								d=" ";
+							}
+						}
+						return metodoNic2(cadena.substring(pos),pos,ap+1);
+					}else if(solucion.size()>0) {
+						apuntador=apuntador+2;
+					}else {
+						apuntador=apuntador+2;
 					}
 				}
+			}
 			if(posicionSi==0) {
 				System.out.print(cadena.substring(0, 1));
 				return metodoNic2(cadena.substring(pos+1),0,0);
@@ -317,7 +237,7 @@ public class Objeto {
 				if(cadena.length()==1) {
 					return metodoNic2(cadena,0,0);
 				}else {
-					return metodoNic2(cadena.substring(posicionSi-1),0,0);
+					return metodoNic2(cadena,0,ap);
 				}
 			}
 		}
@@ -326,23 +246,4 @@ public class Objeto {
 		}
 		return cadena;
 	}
-
-	
-	
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	public void imprimir() {
-		for(int x=0;x<solucion.size();x++) {
-			Objeto temporal=solucion.get(x);
-			if(temporal.solucion.isEmpty()) {
-				if(temporal.repetidorMulti!=null) {
-					temporal.repetidorMulti.imprimir();
-				}else
-				System.out.println("EXPRESION:  " +temporal.expresion+"  REPETIDOR:  "+temporal.repetidor);
-			}else {
-				System.out.println("\t");
-				temporal.imprimir();
-			}
-		}
-	}
-
 }
--- PROYECTO/Orquestador.java
+++ PROYECTO/Orquestador.java
@@ -1,25 +1,11 @@
-//CLASE QUE CONTIENE EL MAIN
-//REALIZA LAS PRIMERAS VALIDACIONES PARA DEJAR LIMPIA TANTO LA CADENA COMO LA EXPRESION ORIGINAL Y ASI EVITAR ERRORES
-//MANDA A LLAMAR A LA CLASE OBJETO
-
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Stack;
 
 public class Orquestador {
-	//3 VARIABLES BASICAS
 	public String cadena;
 	public String reemplazo;
 	public String ER;
-	
-	
-	//LISTA PARA MANIPULAR NUESTRA CADENA
 	public List<String> palabras=new ArrayList<String>();
-
-
-	//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	// VALIDACIONES GENERALES
-	// VALIDACIONES CON EL PROPOSITO DE EVITAR QUE NUESTRO CODIGO TRUENE POR "ERRORES" DE ESCRITURA
 
 	public boolean validaCadena(){   //VALIDACION DE CADENA NO VACIA
 		if(cadena.isEmpty()){
@@ -51,35 +37,6 @@ public class Orquestador {
 			}	
 		}
 	}
-
-	///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	//VALIDAR PARENTESIS      
-	//VALIDACION REALIZADA PARA CHECAR QUE LOS PARENTESIS ESTEN COLOCADOS DE MANERA CORRECTA Y EVITAR QUE TRUENE EL PROGRAMA 
-	public boolean validarMalParentesis() {
-
-		Stack<Character> stackLetras = new Stack<Character>();  //USO UN STACK PARA GUARDAR LOS PARENTESIS
-		char arrayDeParentesis[] = ER.toCharArray();  //CONVIERTO MI ER EN UN ARREGLO
-		
-		int i;
-		for (i = 0; i < arrayDeParentesis.length; i++) {
-			if (arrayDeParentesis[i] == '(') {
-				stackLetras.push(arrayDeParentesis[i]);  //CADA QUE ENCUENTRA UN PARENTESIS QUE ABRE LO GUARDA EN EL STACK
-			}else if (arrayDeParentesis[i] == ')') { 
-				if (!stackLetras.empty() && stackLetras.peek() != ')') {
-					stackLetras.pop();  //CADA QUE ENCUENTRA UN PARENTESIS QUE CIERRA Y EL STACK DE PARENTESIS NO ESTA VACIO SACA EL PARENTESIS QUE ABRE
-				}else{
-					stackLetras.push(arrayDeParentesis[i]);  
-				}
-			}
-		}										 //SI EL STACK ESTA VACIO SIGNIFICA QUE HAY EL MISMO NUMERO DE PARENTESIS QUE ABRE QUE LOS QUE CIERRA
-		return stackLetras.empty();  //POR LO TANTO REGRESA TRUE SI ESTA BIEN, DE LO CONTRARIO FALSE
-
-	}
-
-
-	//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	//VALIDAR EXPRESION REGULAR -> NO INICIE CON +/* O QUE TERMINE CON + O QUE DESPUES DE UNA + HAYA UN *
-	//VALIDACION PARA EVITAR "ERRORES" DE ESCRITURA INICIALES EN NUESTRA EXPRESION REGULAR 
 	
 	public boolean validar_esp(){
 		if(ER.charAt(0)==('*')&&ER.charAt(0)==('+')&&ER.charAt(ER.length()-1)==('+')) {
@@ -95,48 +52,35 @@ public class Orquestador {
 		}
 		return false;
 	}
-	
-	//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	//METODO INVOCADOR LA PRIMERA VEZ 
-	//		->CONTIENE LOS VALORES INICIALES 
-	//		->LLEVA A CABO TODAS LAS VALIDACIONES INICIALES
-	//		->MANDA A LLAMAR A MI CLASE OBJETO 
-	
+ 	
 	public void invocador() {
-		//cadena="abaa aab aaanbbb";  //xAx   //hey
-		cadena="xaccx";
+		//cadena="abaa aab aaanbbb";  
+		//cadena="abaaaabaaanbbb";
+		//cadena="xacccDaeeeDx";
+		//cadena="xaDaaeex";
+		cadena="xaDaeeex";
+		//cadena="CxaDDxsxscxxrrrrr";
 		reemplazo="w";
-		//ER="aaba*+bbb*";
-		ER="a+ccb*";
+		//reemplazo="ccc";
+		//ER="aba*+bbb*";
+		ER="ac*Dae*";
+		//ER="C+aD*+sc+r*";
 
-		if(validar_esp()||validarMalParentesis()==false||validaCadena()) {   //SI HAY ALGUN ERROR NO LO EJECUTA
-			if(validarMalParentesis()==false) {
-				System.out.println("HAY UN ERROR EN TUS PARENTESIS");
-			}
-			return;
-		}else {
-			quitarSalto();
-			quitarTab();
-			quitarEspaciosER();
-			cadenaLista();
-			Objeto objeto=new Objeto(ER);
-			objeto.reemplazo=this.reemplazo;
-			//objeto.imprimir();
-			for(int x=0;x<palabras.size();x++) {
-				objeto.metodoNic(palabras.get(x));   //METODO REPLACE
-				//System.out.print(palabras.get(x));
-				System.out.println("");
-			}
+		quitarSalto();
+		quitarTab();
+		quitarEspaciosER();
+		cadenaLista();
+		Objeto objeto=new Objeto(ER);
+		objeto.reemplazo=this.reemplazo;
+	
+		for(int x=0;x<palabras.size();x++) {
+			objeto.metodoNic(palabras.get(x));   //METODO REPLACE
+			System.out.println("");
 		}
 	}
-	
 
-	//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	//METODO MAIN
-	//MANDO A LLAMAR A MI METODO INVOCADOR 
 	public static void main(String[] args) {
 		Orquestador  coso=new Orquestador();
 		coso.invocador();		
 	}
-	
 }
